e483e37cf9eb39fdbb1d54bbd3585894da85deb5
Steven Tilley <steve@steventilley.com>
2019-10-25 11:43:39 -0400

    add custom shear checking

--- python/pndni/forceqform.py
+++ python/pndni/forceqform.py
@@ -1,5 +1,6 @@
 import argparse
 import nibabel
+import numpy as np
 from pathlib import Path
 
 
@@ -11,10 +12,26 @@ def get_parser():
                         help='Input file')
     parser.add_argument('output_file', type=Path,
                         help='Output file')
+    parser.add_argument('--maxangle', type=float, default=1e-6,
+                        help='The maximum permissible angle (in radians) that '
+                             'the differences in the coordinate axes are permitted '
+                             'to deviate from 90 degres. In other words limits '
+                             'the shearing in the input affine (The qform cannot '
+                             'encode shear.')
     return parser
 
 
-def forceqform(input_file, output_file):
+def _ang(v1, v2):
+    return np.abs(np.arccos(np.dot(v1, v2) / np.linalg.norm(v1, ord=2) / np.linalg.norm(v2, ord=2)) - np.pi / 2.0)
+
+
+def _check_ang(R, maxang):
+    angs = np.array([_ang(R[:, i], R[:, j]) for i, j in [(0, 1), (0, 2), (1, 2)]])
+    if np.any(angs > maxang):
+        raise RuntimeError('Too much shear present. Try increasing maxangle')
+
+
+def forceqform(input_file, output_file, maxangle=1e-6):
     """
     Ensure that only qform is set. Because ANTs only uses the qform, this will
     force other applications to use the qform as well, keeping everything consistent.
@@ -23,8 +40,7 @@ def forceqform(input_file, output_file):
     * If both codes are > 0, set the sform code to 0
     * if the qform is 0 and the sform code is > 0, set the qform based on the
       affine matrix as determined by nibabel (which will be based on the sform),
-      and set sform code to 0. The function :py:func:`set_sform` from :py:mod:`nibabel`
-      is used, with the strip_shears option set to False.
+      and set sform code to 0. Uses the function :py:func:`set_sform` from :py:mod:`nibabel`
     * If both codes are 0, raise a RuntimeError
     """
     img = nibabel.load(str(input_file))
@@ -36,7 +52,8 @@ def forceqform(input_file, output_file):
     elif qcode > 0 and scode > 0:
         img.set_sform(None)
     elif qcode == 0 and scode > 0:
-        img.set_qform(img.affine, code=scode, strip_shears=False)
+        _check_ang(img.affine[:3, :3], maxangle)
+        img.set_qform(img.affine, code=scode)
         img.set_sform(None)
     else:
         raise RuntimeError('Neither sform and qform set')
@@ -45,7 +62,7 @@ def forceqform(input_file, output_file):
 
 def main():
     args = get_parser().parse_args()
-    forceqform(args.input_file, args.output_file)
+    forceqform(args.input_file, args.output_file, maxangle=args.maxangle)
 
 
 if __name__ == '__main__':
--- tests/test_forceqform.py
+++ tests/test_forceqform.py
@@ -4,13 +4,47 @@ import numpy as np
 import pytest
 
 
-@pytest.mark.parametrize('testtype', ['qform', 'sform', 'both', 'none'])
-def test_forceqform(tmp_path, testtype):
+def test_ang():
+    x = np.array([1.0, 0.0, 0.0])
+    y = np.array([0.0, 2.0, 0.0])
+    z = np.array([0.0, 0.0, 3.0])
+    for i in [x, y, z]:
+        for j in [x, y, z]:
+            if i is j:
+                assert np.allclose(forceqform._ang(i, j), np.pi / 2)
+            else:
+                assert np.allclose(forceqform._ang(i, j), 0)
+
+
+def test_check_ang():
+    R1 = np.array([[1.0, -2.0, 0.0],
+                   [1.0, 2.0, 0.0],
+                   [0.0, 0.0, 3.0]])
+    forceqform._check_ang(R1, 1e-6)
+    R2 = np.array([[1.0, 0.0, 0.0],
+                   [1.0, 2.0, 0.0],
+                   [0.0, 0.0, 3.0]])
+    with pytest.raises(RuntimeError):
+        forceqform._check_ang(R2, 1e-6)
+    with pytest.raises(RuntimeError):
+        forceqform._check_ang(R2, np.pi / 4 - 0.01)
+    forceqform._check_ang(R2, np.pi / 4 + 0.01)
+
+
+@pytest.mark.parametrize('testtype,shear,maxang', [('qform', False, None),
+                                                   ('sform', False, None),
+                                                   ('both', False, None),
+                                                   ('none', False, None),
+                                                   ('sform', True, None),
+                                                   ('sform', True, np.pi)])
+def test_forceqform(tmp_path, testtype, shear, maxang):
     i1 = tmp_path / 'image1.nii'
     affine = np.array([[1.0, 0.0, 0.0, -20.0],
                        [0.0, 2.0, 0.0, -30.0],
                        [0.0, 0.0, 4.0, -40.0],
                        [0.0, 0.0, 0.0, 1.0]])
+    if shear:
+        affine[0, 2] = 2.0
     img = np.arange(24).reshape(2, 3, 4)
     nii = nibabel.Nifti1Image(img, None)
 
@@ -27,14 +61,18 @@ def test_forceqform(tmp_path, testtype):
         raise RuntimeError()
     nii.to_filename(str(i1))
     parser = forceqform.get_parser()
-    args = parser.parse_args([str(i1), str(tmp_path / 'out.nii')])
-    if testtype != 'none':
-        forceqform.forceqform(args.input_file, args.output_file)
+    toparse = [str(i1), str(tmp_path / 'out.nii')]
+    if maxang is not None:
+        toparse.extend(['--maxangle', str(maxang)])
+    args = parser.parse_args(toparse)
+    if testtype != 'none' and not (shear and maxang is None):
+        forceqform.forceqform(args.input_file, args.output_file, maxangle=args.maxangle)
     else:
         with pytest.raises(RuntimeError):
-            forceqform.forceqform(args.input_file, args.output_file)
+            forceqform.forceqform(args.input_file, args.output_file, maxangle=args.maxangle)
         return
     nout = nibabel.load(str(args.output_file))
-    assert np.all(nout.affine == affine)
-    assert np.all(nout.get_qform() == affine)
+    if maxang is None:
+        assert np.all(nout.affine == affine)
+        assert np.all(nout.get_qform() == affine)
     assert nout.get_sform(coded=True)[1] == 0
